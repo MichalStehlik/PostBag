ff2edf30f2c8d31a851c0f265bfcbb9a23dcfd19
Michal Stehlík <36696240+MichalStehlik@users.noreply.github.com>
2019-11-22 08:01:11 +0100

    Add project files.

---
+++ PostBag/Model/Bag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostBag.Model
+{
+    class Bag
+    {
+        public List<Consignment> Deliveries { get; } = new List<Consignment>();
+        public List<IValuable> Valuables { get; } = new List<IValuable>();
+
+        public void Add(Consignment item)
+        {
+            Deliveries.Add(item);
+            if (item is IValuable)
+            {
+                Valuables.Add((item as IValuable));
+            }
+        }
+
+        public void Add(PostalOrder item)
+        {
+            Valuables.Add(item);
+        }
+    }
+}
---
+++ PostBag/Program.cs
@@ -0,0 +1,35 @@
+﻿using PostBag.Model;
+using System;
+
+namespace PostBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bag bag = new Bag();
+            bag.Add(new Letter("Alice", "Bob"));
+            bag.Add(new Letter("Charles", "Diane", true));
+            bag.Add(new Package("Ethan", "Flora", 10));
+            bag.Add(new Package("Gwen", "Henry", 10, true));
+            bag.Add(new Postcard("Igor", "Jonas"));
+            bag.Add(new ValuableLetter("Kate", "Liam", 1000));
+            bag.Add(new ValuableLetter("Martin", "Nathan", 1000, true));
+            bag.Add(new ValuablePackage("Olivia", "Peter", 10, 1000));
+            bag.Add(new ValuablePackage("Quinn", "Ronald", 10, 1000, true));
+            bag.Add(new PostalOrder("Simon", 1000));
+
+            Console.WriteLine("Zásilky");
+            foreach (var item in bag.Deliveries)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Cennosti");
+            foreach (var item in bag.Valuables)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
